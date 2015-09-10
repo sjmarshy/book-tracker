f43e73cdf06a706ec806007069b3f21e1a354997
Sam <sjmarshy@gmail.com>
2015-09-10 21:47:08 +0100

    first few steps

--- src/cljs/book_tracker/core.cljs
+++ src/cljs/book_tracker/core.cljs
@@ -5,3 +5,42 @@
 (enable-console-print!)
 
 
+
+(defonce app-state (atom {:books [
+                    {:title "Test Book"}
+                    {:title "Test Book2"}
+                    {:title "Test Book3"}
+                    {:title "Test Book4"}]}))
+
+
+(defn add-book [app title]
+ (om/transact! app :books #(conj (:books app) {:title title})))
+
+
+
+(defn book-title [book]
+  (dom/li nil (:title book)))
+
+(defn book-titles [books]
+  (apply dom/ul nil
+    (map book-title books)))
+
+(defn add-book-form [app]
+  (dom/div nil
+    (dom/input #js{:type "input"})
+    (dom/button #js{:onClick #(.log js/console "hi")} "add book")))
+
+
+(defn main []
+  (.log js/console "hi")
+  (om/root
+    (fn [app owner]
+      (reify
+        om/IRender
+        (render [_]
+          (dom/div nil
+          (add-book-form app)
+          (book-titles  (:books app))))))
+    app-state
+    {:target (. js/document (getElementById "app"))}))
+
